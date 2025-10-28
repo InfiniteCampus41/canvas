56ab118426a1290639a376b0b4fc365c09683f4a
Hacker41 <199835595+Ryann-4@users.noreply.github.com>
2025-10-28 21:59:45 +0000

    added pfps to chat

--- settings.js
+++ settings.js
@@ -260,17 +260,17 @@ profilePicBtn.style.background = "black";
 profilePicBtn.style.color = "white";
 profilePicBtn.style.cursor = "pointer";
 const profileImages = [
-    "/pfps/1.jpeg",
-    "/pfps/2.jpeg",
-    "/pfps/3.jpeg",
-    "/pfps/4.jpeg",
-    "/pfps/5.jpeg",
-    "/pfps/6.jpeg",
-    "/pfps/7.jpeg",
-    "/pfps/8.jpeg",
-    "/pfps/9.jpeg",
-    "/pfps/f3.jpeg",
-    "/pfps/kaiden.png"
+    "canvas/pfps/1.jpeg",
+    "canvas/pfps/2.jpeg",
+    "canvas/pfps/3.jpeg",
+    "canvas/pfps/4.jpeg",
+    "canvas/pfps/5.jpeg",
+    "canvas/pfps/6.jpeg",
+    "canvas/pfps/7.jpeg",
+    "canvas/pfps/8.jpeg",
+    "canvas/pfps/9.jpeg",
+    "canvas/pfps/f3.jpeg",
+    "canvas/pfps/kaiden.png"
 ];
 const restrictedPics = [6, 7, 8];
 let currentPicIndex = 0;
